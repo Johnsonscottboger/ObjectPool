3702a4199667dc3f5fe34dabd03195f587280de0
Aning <aning1995@outlook.com>
2017-07-14 10:17:38 +0800

    添加泛型版本

---
+++ ObjectPool/ObjectPool.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPool
+{
+    /// <summary>
+    /// 对象池
+    /// </summary>
+    public static class ObjectPool
+    {
+        /// <summary>
+        /// 对象池中最小数量
+        /// </summary>
+        private const int _minSize = 2;
+
+        /// <summary>
+        /// 对象池中最大数量
+        /// </summary>
+        private const int _maxSize = 100;
+
+        /// <summary>
+        /// 对象池
+        /// </summary>
+        private static ConcurrentQueue<object> _connQueue = new ConcurrentQueue<object>();
+        
+        /// <summary>
+        /// 静态构造方法,初始化对象池
+        /// </summary>
+        static ObjectPool()
+        {
+            foreach (var index in Enumerable.Range(0, _minSize))
+            {
+                _connQueue.Enqueue(CreateNewInstance());
+            }
+        }
+
+        /// <summary>
+        /// 从对象池中获取一个可用对象
+        /// </summary>
+        /// <returns>可用对象</returns>
+        public static Object GetInstance()
+        {
+            Object instance = null;
+            if (!_connQueue.TryDequeue(out instance))
+            {
+                instance = CreateNewInstance();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// 释放
+        /// </summary>
+        /// <param name="obj">释放的对象</param>
+        public static void Release(this Object obj)
+        {
+            if (obj == null)
+                return;
+            if (_connQueue.Count < _maxSize)
+            {
+                if (_connQueue.Any(p => p == obj))
+                {
+                    //抛出此异常,表明此对象池实现不正确
+                    throw new Exception("同一个对象被同时使用");
+                }
+                _connQueue.Enqueue(obj);
+            }
+        }
+
+        /// <summary>
+        /// 创建新对象
+        /// </summary>
+        /// <returns>一个新的对象</returns>
+        private static Object CreateNewInstance()
+        {
+            //具体创建一个新对象的方法
+            return new object();
+        }
+    }
+
+    /// <summary>
+    /// 对象池
+    /// </summary>
+    /// <typeparam name="T">对象类型</typeparam>
+    public static class ObjectPool<T>
+    {
+        /// <summary>
+        /// 对象池中最小数量
+        /// </summary>
+        private const int _minSize = 2;
+
+        /// <summary>
+        /// 对象池中最大数量
+        /// </summary>
+        private const int _maxSize = 100;
+
+        /// <summary>
+        /// 对象池
+        /// </summary>
+        private static ConcurrentQueue<T> _connQueue = new ConcurrentQueue<T>();
+
+        /// <summary>
+        /// 静态构造方法,初始化对象池
+        /// </summary>
+        static ObjectPool()
+        {
+            foreach (var index in Enumerable.Range(0, _minSize))
+            {
+                _connQueue.Enqueue(CreateNewInstance());
+            }
+        }
+
+        /// <summary>
+        /// 从对象池中获取一个可用对象
+        /// </summary>
+        /// <returns>可用对象</returns>
+        public static T GetInstance()
+        {
+            T instance = default(T);
+            if (!_connQueue.TryDequeue(out instance))
+            {
+                instance = CreateNewInstance();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// 释放
+        /// </summary>
+        /// <param name="obj">释放的对象</param>
+        public static void Release(T obj)
+        {
+            if (obj == null)
+                return;
+            if (_connQueue.Count < _maxSize)
+            {
+                if (_connQueue.Any(p => p.Equals(obj)))
+                {
+                    //抛出此异常,表明此对象池实现不正确
+                    throw new Exception("同一个对象被同时使用");
+                }
+                _connQueue.Enqueue(obj);
+            }
+        }
+
+        /// <summary>
+        /// 创建新对象
+        /// </summary>
+        /// <returns>一个新的对象</returns>
+        private static T CreateNewInstance()
+        {
+            //TODO:具体创建一个新对象的方法
+            return default(T);
+        }
+    }
+}
--- ObjectPool/Program.cs
+++ ObjectPool/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ObjectPool
@@ -10,6 +13,69 @@ namespace ObjectPool
     {
         static void Main(string[] args)
         {
+            #region 非泛型版本
+            var tasks = new List<Task>();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            foreach(var index in Enumerable.Range(0,100000))
+            {
+                tasks.Add(new Task(() =>
+                {
+                    //获取一个实例
+                    var obj = ObjectPool.GetInstance();
+
+                    Console.WriteLine($"[{index}]\tobj:{obj.GetHashCode()}");
+
+                    //释放实例
+                    //ObjectPool.Release(obj);
+                    obj.Release();  //扩展方式形式
+
+                }));
+            }
+
+            tasks.ForEach(t =>
+            {
+                t.Start();
+            });
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"运行时间：{sw.ElapsedMilliseconds / 1000}s");
+            #endregion
+
+            #region 泛型版本
+            var tasks1 = new List<Task>();
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+
+            foreach (var index in Enumerable.Range(0, 100000))
+            {
+                tasks1.Add(new Task(() =>
+                {
+                    //获取一个实例
+                    var dbConnection = ObjectPool<IDbConnection>.GetInstance();
+
+                    Console.WriteLine($"[{index}]\tobj:{dbConnection?.GetHashCode()}");
+
+                    //释放实例
+                    ObjectPool<IDbConnection>.Release(dbConnection);
+
+                }));
+            }
+
+            tasks1.ForEach(t =>
+            {
+                t.Start();
+            });
+
+            Task.WaitAll(tasks1.ToArray());
+
+            Console.WriteLine($"运行时间：{sw1.ElapsedMilliseconds / 1000}s");
+            #endregion
+
+
+            Console.ReadLine();
         }
     }
 }
